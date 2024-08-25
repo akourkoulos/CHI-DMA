5f80385dec2bc49f1aa571c2246ebf069b214d00
akourkoulos <kourkaggelos@gmail.com>
2024-08-25 14:39:12 +0300

    add drawings

--- CHI-DMA/CHI-DMA.runs/synth_1/runme.sh
+++ CHI-DMA/CHI-DMA.runs/synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=F:/VivadoInstallation/vivadoInst/Vivado/2019.2/ids_lite/ISE/bin/nt64;F:/VivadoInstallation/vivadoInst/Vivado/2019.2/ids_lite/ISE/lib/nt64:F:/VivadoInstallation/vivadoInst/Vivado/2019.2/bin
+  PATH=C:/Xilinx/Vivado/2019.2/Vivado/2019.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2019.2/Vivado/2019.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2019.2/Vivado/2019.2/bin
 else
-  PATH=F:/VivadoInstallation/vivadoInst/Vivado/2019.2/ids_lite/ISE/bin/nt64;F:/VivadoInstallation/vivadoInst/Vivado/2019.2/ids_lite/ISE/lib/nt64:F:/VivadoInstallation/vivadoInst/Vivado/2019.2/bin:$PATH
+  PATH=C:/Xilinx/Vivado/2019.2/Vivado/2019.2/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2019.2/Vivado/2019.2/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2019.2/Vivado/2019.2/bin:$PATH
 fi
 export PATH
 
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/Aggelos/Desktop/github/CHI-DMA/CHI-DMA/CHI-DMA/CHI-DMA.runs/synth_1'
+HD_PWD='C:/Users/Aggelos/Documents/GitHub/CHI-DMA/CHI-DMA/CHI-DMA.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/xsim_webtalk.tcl
+++ CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/xsim_webtalk.tcl
@@ -1,10 +1,10 @@
-webtalk_init -webtalk_dir C:/Users/Aggelos/Desktop/github/CHI-DMA/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/Aggelos/Documents/GitHub/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Sep  1 17:46:38 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jul 22 23:46:12 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
-webtalk_add_data -client project -key registration_id -value "211686437_0_0_363" -context "software_version_and_target_device"
+webtalk_add_data -client project -key registration_id -value "211686437_0_0_167" -context "software_version_and_target_device"
 webtalk_add_data -client project -key tool_flow -value "xsim_vivado" -context "software_version_and_target_device"
 webtalk_add_data -client project -key beta -value "FALSE" -context "software_version_and_target_device"
 webtalk_add_data -client project -key route_design -value "FALSE" -context "software_version_and_target_device"
@@ -12,9 +12,9 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "42ac37a6-667b-4df3-810b-19ee81d00cb0" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "25a862be-839d-4316-a6e7-12457ce0dfef" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "c7a29f15992b426cb919dc086fd6b26c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "648" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "654" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-2820QM CPU @ 2.30GHz" -context "user_environment"
@@ -22,12 +22,10 @@ webtalk_add_data -client project -key cpu_speed -value "2295 MHz" -context "user
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
-webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
-webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1331300 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "0 ps" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "67.53_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "264416_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1395010753 -regid "211686437_0_0_363" -xml C:/Users/Aggelos/Desktop/github/CHI-DMA/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Aggelos/Desktop/github/CHI-DMA/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Aggelos/Desktop/github/CHI-DMA/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "11476_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2651313142 -regid "211686437_0_0_167" -xml C:/Users/Aggelos/Documents/GitHub/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Aggelos/Documents/GitHub/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Aggelos/Documents/GitHub/CHI-DMA/CHI-DMA/CHI-DMA.sim/sim_1/behav/xsim/xsim.dir/TestFULLSystem_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
